f8195f56b2f8e66820d4d2ebf467a3e1df6f2746
nachoNota <nachonota13@gmail.com>
2024-03-27 21:06:48 -0300

    Agrego ejercicio de punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 main(){
-    printf("Hola mundo");
+    int valor = 10, *punt;
+    punt = &valor;
+
+    printf("\nEl contenido del puntero es: %d", *punt);
+    printf("\nLa direccion de memoria almacenada por el puntero es %p", punt);
+    printf("\nLa direccion de memoria de la variable es %p", &valor);
+    printf("\nLa direccion de memoria del puntero es %p", &punt);
+    printf("\nEl tamaño de memoria utilizado por la variable es %d", sizeof(valor));
+
 }
